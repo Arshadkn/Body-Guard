d0c5b8ca341f2ddc2fab2044605dc167a450c1a5
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-02 03:13:51 +0530

    Update Mods.py

--- plugins/Mods.py
+++ plugins/Mods.py
@@ -63,10 +63,10 @@ async def forward(bot, message):
 
 # show alert
 
-@Client.on_message(filters.edited)
-async def edited(bot,message):
-	chatid= message.chat.id	
-	await bot.send_message(text=f"{message.from_user.mention} Edited This [Message]({message.link})",chat_id=chatid)
+# @Client.on_message(filters.edited)
+# async def edited(bot,message):
+# 	chatid= message.chat.id	
+#	await bot.send_message(text=f"{message.from_user.mention} Edited This [Message]({message.link})",chat_id=chatid)
 
 
 @Client.on_message(filters.via_bot & filters.group)
