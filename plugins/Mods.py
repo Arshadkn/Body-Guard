4746318e622a5b57e21b765ea65096e94a50aac9
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-02 01:40:14 +0530

    Create Mods.py

---
+++ plugins/Mods.py
@@ -0,0 +1,28 @@
+import pyrogram
+import os
+from pyrogram import Client, filters, enums
+from pyrogram.types import Message, User
+
+bughunter0 = Client(
+    "botname",
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"],
+)
+
+
+@Client.on_message(filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text("Start Message Here")
+
+
+
+@Client.on_message(filters.forwarded and filters.channel)
+async def channeltag(bot, message):
+   try:
+       forward_msg = await message.copy(message.chat.id)
+       await message.delete()
+   except:
+       await message.reply_text("Oops , Recheck My Admin Permissions & Try Again")
+
+
