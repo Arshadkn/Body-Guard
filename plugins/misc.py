d1af50f577e256d40ea3c3a2efad59a12e908933
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-02 03:16:25 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -2,7 +2,7 @@ import os
 from pyrogram import Client, filters, enums
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant, MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 # from info import 
-from utils import extract_user, get_file_id, get_poster, last_online
+from utils import extract_user, last_online
 import time
 from datetime import datetime
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
