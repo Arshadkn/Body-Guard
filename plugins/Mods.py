43e2b0039c771327361a71405a855ac57e140d8f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 03:38:00 +0530

    Update Mods.py

--- plugins/Mods.py
+++ plugins/Mods.py
@@ -96,7 +96,7 @@ async def nolink(client: Client,  message):
             except:
                 pass
 	    
-            k = await message.reply_photo(
+            k = await client.send_photo(
 		chat_id=message.chat.id,
                 photo=random.choice(PICS),
                 caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention},\n\n..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
