c820b91db08d586e784a2bee291cb48a9443c862
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 01:48:04 +0530

    Update Mods.py

--- plugins/Mods.py
+++ plugins/Mods.py
@@ -99,7 +99,7 @@ async def nolink(client: Client,  message):
                 parse_mode=enums.ParseMode.HTML
             )
 
-             = await client.send_photo(
+            m = await client.send_photo(
 		chat_id=ADMIN,
                 photo=random.choice(PICS),
                 caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention},\n\n..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
