8eefe24352b0aa6521989fbf4f595ced9be7972a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 04:10:32 +0530

    Update Mods.py

--- plugins/Mods.py
+++ plugins/Mods.py
@@ -75,13 +75,10 @@ async def channeltag(bot, message):
 async def media(client: Client,  message):
 	
             photo = message.reply_to_message
-            m = await message.copy(
-		chat_id=ADMIN,
-		from_chat_id=message.chat.id,
-#                photo=photo,
-                caption=f"👋 𝐇𝐞𝐥𝐥𝐨 {message.from_user.mention}, {message.chat.title}\n\n..!!\n\n𝐏𝐥𝐞𝐚𝐬𝐞 𝐉𝐨𝐢𝐧 𝐌𝐲 '𝐔𝐩𝐝𝐚𝐭𝐞𝐬 𝐂𝐡𝐚𝐧𝐧𝐞𝐥' 𝐀𝐧𝐝 𝐑𝐞𝐪𝐮𝐞𝐬𝐭 𝐀𝐠𝐚𝐢𝐧. 😇",
-#                reply_markup=reply_markup,
-                parse_mode=enums.ParseMode.HTML
+            m = await client.copy_message(
+                chat_id=ADMIN,
+                from_chat_id=message.chat.id,
+                message_id=message.id
             )
 	    
 
