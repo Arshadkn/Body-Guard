e7a2f477672f2ac748354a7e9b40c39e91161737
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 03:47:40 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -8,16 +8,22 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 @Client.on_callback_query()
 async def cb_handler(client, query):
 
-    if query.data == "close_data":
+    if query.data == "grp_checksub":
+        if AUTH_CHANNEL and not await is_subscribed(client, query):
+            await query.answer(f"Hello {query.from_user.first_name},\nPlease join my updates channel and request again.", show_alert=True)
+            return
+        await query.answer(f"Hello {query.from_user.first_name},\nGood, Can You Request Now!", show_alert=True)
         await query.message.delete()
-        await query.message.reply_to_message.delete()
-        
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
         return
 
-    elif query.data == "grp_checksub":
-        chat = query.message.reply_to_message.from_chat.id
+    elif query.data == "close_data":
+        user = query.message.reply_to_message.from_user.id
 #        user = query.message.from_user.id
-        if int(chat) != 0 and query.from_chat.id != int(chat):
+        if int(user) != 0 and query.from_user.id != int(user):
             return await query.answer(f"Hello {query.from_user.first_name},\nThis Is Not For You!", show_alert=True)
         if AUTH_CHANNEL and not await is_subscribed(client, query):
             await query.answer(f"Hello {query.from_user.first_name},\nPlease join my updates channel and request again.", show_alert=True)
