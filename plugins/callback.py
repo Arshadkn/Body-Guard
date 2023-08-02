d2b5d6a832fc61834a91181d8f7f097309a839b0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-02 22:10:58 +0530

    Create callback.py

---
+++ plugins/callback.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, Munni
+from utils import is_subscribed
+from info import AUTH_CHANNEL
+
+@Munni.on_callback_query()
+async def cb_handler(client, query):
+
+    if query.data == "close_data":
+        await query.message.delete()
+        
+        return
+
+    elif query.data == "grp_checksub":
+        user = query.message.reply_to_message.from_user.id
+        if int(user) != 0 and query.from_user.id != int(user):
+            return await query.answer(f"Hello {query.from_user.first_name},\nThis Is Not For You!", show_alert=True)
+        if AUTH_CHANNEL and not await is_subscribed(client, query):
+            await query.answer(f"Hello {query.from_user.first_name},\nPlease join my updates channel and request again.", show_alert=True)
+            return
+        await query.answer(f"Hello {query.from_user.first_name},\nGood, Can You Request Now!", show_alert=True)
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except:
+            pass
