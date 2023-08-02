ebeb13b8de7e38ef92f3cf78fe2d46b6e13883a8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-02 19:14:52 +0530

    Update Mods.py

--- plugins/Mods.py
+++ plugins/Mods.py
@@ -6,6 +6,20 @@ from datetime import datetime, timedelta
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, User, Message, ChatPermissions, CallbackQuery
 
 
+import asyncio
+import os
+import shlex
+from datetime import datetime
+from os.path import basename
+
+import requests
+import tracemoepy
+from bs4 import BeautifulSoup
+from pyrogram import filters
+from typing import Tuple, Optional
+
+
+
 
 
 bughunter0 = Client(
@@ -34,6 +48,65 @@ All Things are Simple To do. Follow The writings given Below.
 
  NB :- BOT WOULD NOT DELETE ADMIN MESSEGES.
 """
+
+
+
+@Client.on_message(filters.me & filters.command(["reverse"], Command))
+async def google_rs(client, message):
+    start = datetime.now()
+    dis_loc = ''
+    base_url = "http://www.google.com"
+    out_str = "`Reply to an image`"
+    if message.reply_to_message:
+        message_ = message.reply_to_message
+        if message_.sticker and message_.sticker.file_name.endswith('.tgs'):
+            await message.delete()
+            return
+        if message_.photo or message_.animation or message_.sticker:
+            dis = await client.download_media(
+                message=message_,
+                file_name=screen_shot
+            )
+            dis_loc = os.path.join(screen_shot, os.path.basename(dis))
+        if message_.animation or message_.video:
+            await message.edit("`Converting this Gif`")
+            img_file = os.path.join(screen_shot, "grs.jpg")
+            await take_screen_shot(dis_loc, 0, img_file)
+            if not os.path.lexists(img_file):
+                await message.edit("`Something went wrong in Conversion`")
+                await asyncio.sleep(5)
+                await message.delete()
+                return
+            dis_loc = img_file
+        if dis_loc:
+            search_url = "{}/searchbyimage/upload".format(base_url)
+            multipart = {
+                "encoded_image": (dis_loc, open(dis_loc, "rb")),
+                "image_content": ""
+            }
+            google_rs_response = requests.post(search_url, files=multipart, allow_redirects=False)
+            the_location = google_rs_response.headers.get("Location")
+            os.remove(dis_loc)
+        else:
+            await message.delete()
+            return
+        await message.edit("`Found Google Result.`")
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:58.0) Gecko/20100101 Firefox/58.0"
+        }
+        response = requests.get(the_location, headers=headers)
+        soup = BeautifulSoup(response.text, "html.parser")
+        prs_div = soup.find_all("div", {"class": "r5a77d"})[0]
+        prs_anchor_element = prs_div.find("a")
+        prs_url = base_url + prs_anchor_element.get("href")
+        prs_text = prs_anchor_element.text
+        end = datetime.now()
+        ms = (end - start).seconds
+        out_str = f"""<b>Time Taken</b>: {ms} seconds
+<b>Possible Related Search</b>: <a href="{prs_url}">{prs_text}</a>
+<b>More Info</b>: Open this <a href="{the_location}">Link</a>
+"""
+    await message.edit(out_str, parse_mode=enums.ParseMode.HTML, disable_web_page_preview=True)
 
 
 
