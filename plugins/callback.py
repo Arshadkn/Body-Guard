02f51fcf9e5d17d04e777ef0932617460c8e3f4b
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 14:58:42 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -21,18 +21,4 @@ async def cb_handler(client, query):
         return
 
     elif query.data == "close_data":
-        await query.message.delete()
-        if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer(f"Hello {query.from_user.first_name},\nPlease join my updates channel and request again.", show_alert=True)
-            return
-        else:
-                    await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
-                await query.answer('Cʜᴇᴄᴋ PM, I ʜᴀᴠᴇ sᴇɴᴛ ғɪʟᴇs ɪɴ PM', show_alert=True)
-        except UserIsBlocked:
-            await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
-        except PeerIdInvalid:
-            await query.answer(url=f"https://telegram.me/{temp.U_NAME}")
-        except Exception as e:
-            await query.answer(url=f"https://telegram.me/{temp.U_NAME}")
-            await query.message.delete()
-           
+        await query.message.reply_to_message.delete()
