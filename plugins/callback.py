81757cfbb7c1a1c4a08ad1a0c3a572bd0dff9815
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 14:57:11 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -21,15 +21,18 @@ async def cb_handler(client, query):
         return
 
     elif query.data == "close_data":
-#        user = query.message.reply_to_message.from_user.id
-#        
+        await query.message.delete()
         if AUTH_CHANNEL and not await is_subscribed(client, query):
             await query.answer(f"Hello {query.from_user.first_name},\nPlease join my updates channel and request again.", show_alert=True)
             return
-    else:
-        await query.answer(f"Hello {query.from_user.first_name},\nGood, Can You Request Now!", show_alert=True)
-        await query.message.reply_to_message.delete()
-        try:
-            await query.message.reply_to_message.delete()
-        except:
-            pass
+        else:
+                    await query.answer(f"Hᴇʏ {query.from_user.first_name}, Tʜɪs Is Nᴏᴛ Yᴏᴜʀ Mᴏᴠɪᴇ Rᴇǫᴜᴇsᴛ. Rᴇǫᴜᴇsᴛ Yᴏᴜʀ's !", show_alert=True)
+                await query.answer('Cʜᴇᴄᴋ PM, I ʜᴀᴠᴇ sᴇɴᴛ ғɪʟᴇs ɪɴ PM', show_alert=True)
+        except UserIsBlocked:
+            await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
+        except PeerIdInvalid:
+            await query.answer(url=f"https://telegram.me/{temp.U_NAME}")
+        except Exception as e:
+            await query.answer(url=f"https://telegram.me/{temp.U_NAME}")
+            await query.message.delete()
+           
