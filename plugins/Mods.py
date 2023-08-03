dd9356c2909cdfd0b8131c022277e4d98a039efa
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-03 06:19:01 +0530

    Update Mods.py

--- plugins/Mods.py
+++ plugins/Mods.py
@@ -50,7 +50,7 @@ invite_link = "https://t.me/testpubliconly"
 
 @Client.on_message(filters.group)
 async def protect(client, message):
-    chat_id = message.cha.id
+    chat_id = message.chat.id
     await client.set_chat_protected_content(chat_id, enabled)
 
 
