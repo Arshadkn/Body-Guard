a3942c7b9f2dbe3756bfceefb12186b4aab2de83
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-08-06 16:38:40 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -57,13 +57,7 @@ async def cb_handler(client, query):
 @Client.on_message(filters.private & filters.command(["img"]))
 async def searchimage(client, message):
     try:
-       args = message.text.split(None, 1)[1]
-    
-    pak = await message.reply('Downloading...')
-    try:
+        args = message.text.split(None, 1)[1]
         r = requests.get(f"https://apibu.herokuapp.com/api/y-images?query={args}&page=1&limit=1").json()
-        slink = r['data']['results'][0]['downloadUrl'][4]['link']
-    
-    
-    
+        slink = r['data']['results'][0]['downloadUrl'][4]['link']    
         await message.reply_message(slink)
